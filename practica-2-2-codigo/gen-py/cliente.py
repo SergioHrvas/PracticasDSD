d25cbb0d939d64f2a117ab6e18fc65ac4a64bf37
Sergio <sergiohervas18@gmail.com>
2022-03-29 15:01:22 +0200

    Avance con python

--- practica-2-2-codigo/gen-py/cliente.py
+++ practica-2-2-codigo/gen-py/cliente.py
@@ -16,9 +16,27 @@ transport.open()
 print("hacemos ping al server")
 client.ping()
 
-resultado = client.suma(1, 1)
-print("1 + 1 = " + str(resultado))
-resultado = client.resta(1, 1)
-print("1 - 1 = " + str(resultado))
+opcion = input();
+while (opcion != 's'):
+    print("=======CALCULADORA========")
+    print("Operaciones simples: s")
+    print("Operaciones con polinomios: p")
+    print("Operaciones con vectores: v")
+    print("Operaciones con matrices: m")
+    print("Salir: q\n")
+    operacion = input()
+    if(operacion == 's'):
+        resultado = client.suma(1, 1)
+        print("1 + 1 = " + str(resultado))
+        resultado = client.resta(1, 1)
+        print("1 - 1 = " + str(resultado))
+    
+    
+    
+    
+    print("")
+    opcion = input()
+
 
 transport.close()
+
