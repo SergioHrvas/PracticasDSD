bbdb4f17223f2083aa5ece642d37a4edf0c60712
Sergio <sergiohervas18@gmail.com>
2022-05-18 01:05:35 +0200

    Temperaturas acabadas

--- P4/Parte_2/conexion.js
+++ P4/Parte_2/conexion.js
@@ -3,19 +3,20 @@ var url = require("url");
 var fs = require("fs");
 var path = require("path");
 var socketio = require("socket.io");
+const { emit } = require("process");
 
 var MongoClient = require('mongodb').MongoClient;
 var MongoServer = require('mongodb').Server;
-var mimeTypes = { "html": "text/html", "jpeg": "image/jpeg", "jpg": "image/jpeg", "png": "image/png", "js": "text/javascript", "css": "text/css", "swf": "application/x-shockwave-flash"};
+var mimeTypes = { "html": "text/html", "jpeg": "image/jpeg", "jpg": "image/jpeg", "png": "image/png", "js": "text/javascript", "css": "text/css", "swf": "application/x-shockwave-flash" };
 
 var httpServer = http.createServer(
-	function(request, response) {
+	function (request, response) {
 		var uri = url.parse(request.url).pathname;
-		if (uri=="/") uri = "/domotica.html";
+		if (uri == "/") uri = "/domotica.html";
 		var fname = path.join(process.cwd(), uri);
-		fs.exists(fname, function(exists) {
+		fs.exists(fname, function (exists) {
 			if (exists) {
-				fs.readFile(fname, function(err, data){
+				fs.readFile(fname, function (err, data) {
 					if (!err) {
 						var extension = path.extname(fname).split(".")[1];
 						var mimeType = mimeTypes[extension];
@@ -24,15 +25,15 @@ var httpServer = http.createServer(
 						response.end();
 					}
 					else {
-						response.writeHead(200, {"Content-Type": "text/plain"});
-						response.write('Error de lectura en el fichero: '+uri);
+						response.writeHead(200, { "Content-Type": "text/plain" });
+						response.write('Error de lectura en el fichero: ' + uri);
 						response.end();
 					}
 				});
 			}
-			else{
-				console.log("Peticion invalida: "+uri);
-				response.writeHead(200, {"Content-Type": "text/plain"});
+			else {
+				console.log("Peticion invalida: " + uri);
+				response.writeHead(200, { "Content-Type": "text/plain" });
 				response.write('404 Not Found\n');
 				response.end();
 			}
@@ -41,27 +42,99 @@ var httpServer = http.createServer(
 );
 
 
-MongoClient.connect("mongodb://localhost:27017/", { useUnifiedTopology: true }, function(err, db) {
+
+
+var ventana = true, aire = true;
+var temperatura = 23, luz = 100;
+var temperatura_fija = 23;
+
+
+
+var allClients = new Array();
+
+MongoClient.connect("mongodb://localhost:27017/", { useUnifiedTopology: true }, function (err, db) {
 	httpServer.listen(8080);
 	var io = socketio(httpServer);
 
-	var dbo = db.db("pruebaBaseDatos");
-	dbo.createCollection("test", function(err, collection){
-    	io.sockets.on('connection',
-		function(client) {
-			client.emit('my-address', {host:client.request.connection.remoteAddress, port:client.request.connection.remotePort});
-			client.on('poner', function (data) {
-				if(collection != null){
-				collection.insertOne(data, {safe:true}, function(err, result) {});
+	setInterval(function () {
+		if (aire) {
+			if (temperatura < temperatura_fija) {
+				temperatura += 1;
 			}
-		});
-			client.on('obtener', function (data) {
-				collection.find(data).toArray(function(err, results){
-					client.emit('obtener', results);
+			else if (temperatura > temperatura_fija) {
+				temperatura -= 1;
+			}
+		}
+		else if (temperatura < 50 && !aire) {
+			temperatura += 1;
+	
+		}
+		var d = new Date();
+		var t = d.getTime();
+		io.sockets.emit('envioTemp',{temperatura,t});
+	}
+		, 5000);
+
+	var dbo = db.db("pruebaBaseDatos");
+	dbo.createCollection("test", function (err, collection) {
+		io.sockets.on('connection',
+			function (client) {
+				client.emit('ack', "Conexión recibida. Te respondo.");
+
+				client.on('ack', function (data) {
+					console.log(data);
+					allClients.push({ address: client.request.connection.remoteAddress, port: client.request.connection.remotePort });
+					io.sockets.emit('all-connections', allClients);
+				});
+
+				client.on('aire', function (data) {
+					console.log(`Aire: ${data.cambio}`);
+					aire = data.cambio;
+					io.sockets.emit('aire', aire);
+				});
+
+				client.on('persiana', function (data) {
+					console.log(`Persiana: ${data.cambio}`);
+					ventana = data.cambio;
+					io.sockets.emit('persiana', ventana);
+				});
+
+				client.on('disconnect', function () {
+					console.log("El cliente " + client.request.connection.remoteAddress + " se va a desconectar");
+					console.log(allClients);
+
+					var index = -1;
+					for (var i = 0; i < allClients.length; i++) {
+						if (allClients[i].address == client.request.connection.remoteAddress
+							&& allClients[i].port == client.request.connection.remotePort) {
+							index = i;
+						}
+					}
+
+					
+					if (index != -1) {
+						allClients.splice(index, 1);
+						io.sockets.emit('all-connections', allClients);
+					} else {
+						console.log("EL USUARIO NO SE HA ENCONTRADO!")
+					}
+					console.log('El usuario ' + client.request.connection.remoteAddress + ' se ha desconectado');
+				});
+
+				client.on('nuevaTemp', function (data) {
+					temperatura = data.temperatura;
+					if (collection != null) {
+						collection.insertOne(data, { safe: true }, function (err, result) { });
+					}
+					io.sockets.emit('envioTemp', data);
 				});
 			});
-		});
-    });
+	});
 });
+
+
+
+
+
 
 console.log("Servicio MongoDB iniciado");
--- P4/Parte_2/domotica.js
+++ P4/Parte_2/domotica.js
@@ -3,75 +3,167 @@ document.getElementById("Persiana").addEventListener('click', function () { even
 document.getElementById("enviar1").addEventListener('click', function () { setTemperatura() });
 document.getElementById("enviar2").addEventListener('click', function () { setLuz() });
 
+var ventana = true, aire = true;
+var temperatura = 23, luz = 100;
 
-
-var ventana = false, aire = false;
-var temperatura = 40, luz = 0;
-setInterval(function(){
-            if (temperatura > 5 && !aire) {
-                temperatura -= 1;
-                document.getElementById("tempsensor").innerHTML = temperatura;
-            }
-            if (luz <= 100 && !ventana) {
-
-                luz += 0.5;
-                document.getElementById("luzsensor").innerHTML = luz;
-
-            }
-    }
-, 10000);
-
-
-function eventoAire(){
-    if(aire){
-        aire = false;
-        document.getElementById("tempsensor").innerHTML = temperatura;
-        document.getElementById("AireAcon").setAttribute("style", "-webkit-filter:grayscale(0%)");
-    }
-    else{
-        aire = true;
-        document.getElementById("AireAcon").setAttribute("style", "-webkit-filter:grayscale(100%)");
-
-    }
-}
-
-function eventoPersiana(){
-    if(!ventana){
-        ventana = true;
-        luz = 0;
-        document.getElementById("luzsensor").innerHTML = luz;
-        document.getElementById("Persiana").setAttribute("style", "-webkit-filter:grayscale(100%)");
-
-    }
-    else{
-        ventana = false;
-        document.getElementById("Persiana").setAttribute("style", "-webkit-filter:grayscale(0%)");
-
-    }
-}
-
-function setTemperatura(){
-    var x = document.getElementById("temp").value;
-    document.getElementById("tempsensor").innerHTML=x;
-}
-
-function setLuz(){
-    var x = document.getElementById("luz").value;
-    document.getElementById("luzsensor").innerHTML=x;
-}
 var serviceURL = document.URL;
 var socket = io.connect(serviceURL);
 
+function alertarTemp() {
+    if (temperatura >= 40 || temperatura <= 0) {
+        document.getElementsByClassName("alerta").item(0).innerHTML = "¡CUIDADO!";
+        document.getElementById("Temperatura").setAttribute("style", "filter: grayscale(100%) brightness(40%) sepia(100%) hue-rotate(-50deg) saturate(600%) contrast(0.8);");
 
-socket.on('my-address', function (data) {
+    }
+    else {
+        document.getElementsByClassName("alerta").item(0).innerHTML = "";
+        document.getElementById("Temperatura").setAttribute("style", "filter: ;");
+    }
+}
+
+function alertarLuz() {
+    if (luz >= 100) {
+        document.getElementsByClassName("alerta").item(1).innerHTML = "¡CUIDADO!";
+        document.getElementById("Luminosidad").setAttribute("style", "filter: grayscale(100%) brightness(40%) sepia(100%) hue-rotate(-50deg) saturate(600%) contrast(0.8);");
+
+    }
+}
+
+function eventoAire() {
+    if (aire) {
+        socket.emit('aire', { cambio: false });
+        //      document.getElementById("tempsensor").innerHTML = temperatura;
+        document.getElementById("AireAcon").setAttribute("style", "-webkit-filter:grayscale(100%)");
+    }
+    else {
+        socket.emit('aire', { cambio: true });
+        document.getElementById("AireAcon").setAttribute("style", "-webkit-filter:grayscale(0%)");
+
+    }
+
+}
+
+function eventoPersiana() {
+    if (!ventana) {
+        socket.emit('persiana', { cambio: true });
+        luz = 100;
+        document.getElementById("luzsensor").innerHTML = luz + "%";
+        document.getElementById("Persiana").setAttribute("style", "-webkit-filter:grayscale(0%)");
+
+    }
+    else {
+        socket.emit('persiana', { cambio: false });
+        luz = 0;
+        document.getElementById("luzsensor").innerHTML = luz + "%";
+        document.getElementById("Persiana").setAttribute("style", "-webkit-filter:grayscale(100%)");
+
+    }
+}
+
+function setTemperatura() {
+    var x = document.getElementById("temp").value;
+    temperatura = parseInt(x);
+    document.getElementById("tempsensor").innerHTML = temperatura;
+    alertarTemp();
+    
+    var t = new Date();
+    t = t.getTime;
+    socket.emit('nuevaTemp', { temperatura, t });
+}
+
+function setLuz() {
+    var x = document.getElementById("luz").value;
+    luz = parseInt(x);
+    document.getElementById("luzsensor").innerHTML = x;
+    alertarLuz();
+}
+
+function actualizarConexiones(usuarios) {
+    var listContainer = document.getElementsByClassName('mensajes').item(2);
+    listContainer.innerHTML = '';
+    var listElement = document.createElement('ol');
+    listContainer.appendChild(listElement);
+    var num = usuarios.length;
+    for (var i = 0; i < num; i++) {
+        var listItem = document.createElement('li');
+        listItem.innerHTML = usuarios[i].address + ":" + usuarios[i].port;
+        listElement.appendChild(listItem);
+    }
+}
+
+
+function actualizarHistorico(temperaturas) {
+    var listContainer = document.getElementsByClassName('mensajes').item(0);
+    listContainer.innerHTML = '';
+    var listElement = document.createElement('ol');
+    listContainer.appendChild(listElement);
+    var num = temperaturas.length;
+    for (var i = 0; i < num; i++) {
+        var listItem = document.createElement('li');
+        listItem.innerHTML = temperaturas[i].t + ":" + temperaturas[i].temperatura;
+        listElement.appendChild(listItem);
+    }
+}
+
+
+socket.on('ack', function (data) {
+    console.log(data);
+    socket.emit('ack', "Conexion establecida correctamente!")
     var d = new Date();
-    socket.emit('poner', { host: data.host, port: data.port, time: d });
-    socket.emit('obtener', { host: data.address });
+    d.getTime();
+    console.log(d);
+
+
+    //
+    //socket.emit('obtener', { host: data.address });
 });
-socket.on('obtener', function (data) {
-    // actualizarLista(data);
+socket.on('all-connections', function (data) {
+    actualizarConexiones(data);
 });
+
 socket.on('disconnect', function () {
     //actualizarLista({});
 });
+
+socket.on('envioTemp', function (data) {
+    temperatura = data.temperatura;
+    document.getElementById("tempsensor").innerHTML = temperatura;
+    alertarTemp();
+
+
+
+    actualizarHistorico(data);
+});
+
+socket.on('persiana', function (data) {
+    ventana = data;
+    console.log(ventana);
+
+    if (!ventana) {
+        luz = 0;
+        document.getElementById("Persiana").setAttribute("style", "-webkit-filter:grayscale(100%)");
+
+    }
+    else {
+        luz = 100;
+        document.getElementById("Persiana").setAttribute("style", "-webkit-filter:grayscale(0%)");
+
+    }
+    document.getElementById("luzsensor").innerHTML = luz + "%";
+
+});
+
+socket.on('aire', function (data) {
+    aire = data;
+    console.log(aire);
+    if (aire) {
+        //      document.getElementById("tempsensor").innerHTML = temperatura;
+        document.getElementById("AireAcon").setAttribute("style", "-webkit-filter:grayscale(0%)");
+    }
+    else {
+        document.getElementById("AireAcon").setAttribute("style", "-webkit-filter:grayscale(100%)");
+
+    }
+});
+
 
