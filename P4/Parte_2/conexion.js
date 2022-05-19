45d0301d6a3dc48ffb7dbefff3bb30c06361661b
Sergio <sergiohervas18@gmail.com>
2022-05-19 14:45:58 +0200

    Basico hecho + Calendario empezado

--- P4/Parte_2/conexion.js
+++ P4/Parte_2/conexion.js
@@ -9,6 +9,9 @@ var MongoClient = require('mongodb').MongoClient;
 var MongoServer = require('mongodb').Server;
 var mimeTypes = { "html": "text/html", "jpeg": "image/jpeg", "jpg": "image/jpeg", "png": "image/png", "js": "text/javascript", "css": "text/css", "swf": "application/x-shockwave-flash" };
 
+
+
+
 var httpServer = http.createServer(
 	function (request, response) {
 		var uri = url.parse(request.url).pathname;
@@ -49,12 +52,30 @@ var temperatura = 23, luz = 100;
 var temperatura_fija = 23;
 
 
-
 var allClients = new Array();
 
 MongoClient.connect("mongodb://localhost:27017/", { useUnifiedTopology: true }, function (err, db) {
 	httpServer.listen(8080);
 	var io = socketio(httpServer);
+
+	function verificarFallos() {
+		if ((temperatura < -10 || temperatura >= 50) && luz > 100) {
+			ventana = false;
+			io.sockets.emit('persiana', ventana);
+			io.sockets.emit('alertaluz', false);
+
+		}
+	}
+
+	function envioAlertaTemp() {
+		var alerta = false;
+		if (temperatura >= 40 || temperatura <= 0) {
+			alerta = true;
+		}
+
+		io.sockets.emit('alertatemp', alerta);
+
+	}
 
 	setInterval(function () {
 		if (aire) {
@@ -67,16 +88,17 @@ MongoClient.connect("mongodb://localhost:27017/", { useUnifiedTopology: true }, 
 		}
 		else if (temperatura < 50 && !aire) {
 			temperatura += 1;
-	
+
 		}
 		var d = new Date();
 		var t = d.getTime();
-		io.sockets.emit('envioTemp',{temperatura,t});
+		io.sockets.emit('envioTemp', { temperatura, t });
+		envioAlertaTemp();
 	}
 		, 5000);
 
 	var dbo = db.db("pruebaBaseDatos");
-	dbo.createCollection("test", function (err, collection) {
+	dbo.createCollection("temps", function (err, collection) {
 		io.sockets.on('connection',
 			function (client) {
 				client.emit('ack', "Conexión recibida. Te respondo.");
@@ -111,7 +133,7 @@ MongoClient.connect("mongodb://localhost:27017/", { useUnifiedTopology: true }, 
 						}
 					}
 
-					
+
 					if (index != -1) {
 						allClients.splice(index, 1);
 						io.sockets.emit('all-connections', allClients);
@@ -126,72 +148,81 @@ MongoClient.connect("mongodb://localhost:27017/", { useUnifiedTopology: true }, 
 					if (collection != null) {
 						collection.insertOne(data, { safe: true }, function (err, result) { });
 						collection.find().toArray(function (err, results) {
-								if (err) {
-									console.log("ERROR");
-								}
-								else {
-									console.log(results[0].temperatura)
-									io.sockets.emit('envioHistorico', results);
-								}
-	
-							});
-	
+							if (err) {
+								console.log("ERROR");
+							}
+							else {
+								console.log(results[0].temperatura)
+								io.sockets.emit('envioHistoricoTemps', results);
+							}
+
+						});
+
 					}
 					io.sockets.emit('envioTemp', data);
+
+					envioAlertaTemp();
+					verificarFallos();
+				});
+
+
+
+			});
+	});
+	dbo.createCollection("luces", function (err, collection) {
+		io.sockets.on('connection',
+			function (client) {
+				client.on('nuevaLuz', function (data) {
+					luz = data.luz;
+					if (collection != null) {
+						collection.insertOne(data, { safe: true }, function (err, result) { });
+						collection.find().toArray(function (err, results) {
+							if (err) {
+								console.log("ERROR");
+							}
+							else {
+								console.log(results[0].luz)
+								io.sockets.emit('envioHistoricoLuces', results);
+							}
+
+						});
+
+					}
+					io.sockets.emit('envioLuz', data);
+					var alerta = false;
+					if (luz >= 100) {
+						alerta = true;
+					}
+					io.sockets.emit('alertaluz', alerta);
+					verificarFallos();
+
 				});
 			});
 	});
+
+
 	dbo.createCollection("chat", function (err, collection) {
 		io.sockets.on('connection',
 			function (client) {
-/*
-				client.on('ack', function (data) {
-					console.log(data);
-					allClients.push({ address: client.request.connection.remoteAddress, port: client.request.connection.remotePort });
-					io.sockets.emit('all-connections', allClients);
-				});
 
-				client.on('aire', function (data) {
-					console.log(`Aire: ${data.cambio}`);
-					aire = data.cambio;
-					io.sockets.emit('aire', aire);
-				});
-
-				client.on('persiana', function (data) {
-					console.log(`Persiana: ${data.cambio}`);
-					ventana = data.cambio;
-					io.sockets.emit('persiana', ventana);
-				});
-
-				client.on('disconnect', function () {
-					console.log("El cliente " + client.request.connection.remoteAddress + " se va a desconectar");
-					console.log(allClients);
-
-					var index = -1;
-					for (var i = 0; i < allClients.length; i++) {
-						if (allClients[i].address == client.request.connection.remoteAddress
-							&& allClients[i].port == client.request.connection.remotePort) {
-							index = i;
-						}
-					}
-
-					
-					if (index != -1) {
-						allClients.splice(index, 1);
-						io.sockets.emit('all-connections', allClients);
-					} else {
-						console.log("EL USUARIO NO SE HA ENCONTRADO!")
-					}
-					console.log('El usuario ' + client.request.connection.remoteAddress + ' se ha desconectado');
-				});
-
-				client.on('nuevaTemp', function (data) {
-					temperatura = data.temperatura;
+				client.on('chat', function (data) {
+					var datos = { msg: data, client: client.request.connection.remotePort }
 					if (collection != null) {
-						collection.insertOne(data, { safe: true }, function (err, result) { });
+						collection.insertOne(datos, { safe: true }, function (err, result) { });
+						collection.find().toArray(function (err, results) {
+							if (err) {
+								console.log("ERROR");
+							}
+							else {
+								console.log(results[0].msg)
+								io.sockets.emit('chat', results);
+							}
+
+						});
+
 					}
-					io.sockets.emit('envioTemp', data);
-				});*/
+				});
+
 			});
 	});
 });
--- P4/Parte_2/domotica.js
+++ P4/Parte_2/domotica.js
@@ -2,15 +2,85 @@ document.getElementById("AireAcon").addEventListener('click', function () { even
 document.getElementById("Persiana").addEventListener('click', function () { eventoPersiana() });
 document.getElementById("enviar1").addEventListener('click', function () { setTemperatura() });
 document.getElementById("enviar2").addEventListener('click', function () { setLuz() });
+document.getElementById("enviarmensaje").addEventListener('click', function () { enviarMensaje(document.getElementById("mensaje"));});
+document.getElementById("mensaje").addEventListener("keyup", function () { evaluarComentario(this, document.getElementById("contadormensaje")) });
 
+document.getElementById("enviarnota").addEventListener('click', function () { enviarMensaje(document.getElementById("nota"));});
+document.getElementById("nota").addEventListener("keyup", function () { evaluarComentario(this, document.getElementById("contadornota")) });
+
+
+//document.getElementById("calendarioenviar").addEventListener('click', function(){ enviarEventoDia(); })
+document.addEventListener('DOMContentLoaded', function(){ pintarMeses()});
+
+let longitudmax = 1000, longitudrestante = 1000;
 var ventana = true, aire = true;
 var temperatura = 23, luz = 100;
+const meses = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
+var diaSeleccionado, mesSeleccionado, añoSeleccionado;
 var serviceURL = document.URL;
 var socket = io.connect(serviceURL);
 
-function alertarTemp() {
-    if (temperatura >= 40 || temperatura <= 0) {
+contar(document.getElementById("enviarmensaje"),document.getElementById("contadormensaje"));
+contar(document.getElementById("enviarnota"), document.getElementById("contadornota"));
+
+setInterval(function(){
+    scroll()
+}, 100);
+
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const mesesNombres = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+function pintarMeses(){
+    var mes = document.getElementById("mes");
+    mes.style.display="grid";
+    mes.style.gridTemplateColumns = "repeat(7,1fr)";
+    var d = new Date();
+    var nummes = d.getMonth();
+    var primerdia = new Date(monthNames[nummes] + " 01, " + d.getFullYear() + " 00:00:01");
+    
+    mesSeleccionado = nummes;
+    añoSeleccionado = d.getFullYear();
+
+    var dia = (primerdia.getDay() + 6) % 7;
+    var diasmes = meses[nummes] + dia;
+    console.log(dia);
+    console.log(diasmes);
+
+    var contador = 1;
+    for(var d = 0; d < diasmes; d++){
+        var diaelement = document.createElement("div");
+        diaelement.style.display="grid";
+        diaelement.style.cursor = "pointer";
+        diaelement.className = "dia";
+
+        if(d < dia){
+            diaelement.innerHTML = " - ";
+        }
+        else{
+            diaelement.innerHTML = contador;
+            contador++;
+            diaelement.addEventListener('click', function(){mostrarform(this.innerHTML)});
+
+        }
+        mes.appendChild(diaelement);
+    }
+}   
+
+
+function mostrarform(dia){
+    diaSeleccionado = parseInt(dia);
+    document.getElementById("cajamensajes").style.display = "initial";
+    fecha = diaSeleccionado + "/" + mesesNombres[mesSeleccionado] + "/" + añoSeleccionado; 
+    document.getElementById("fecha").innerHTML = fecha;
+}
+
+function evaluarComentario(elemento, contador) {
+    censurar(elemento);
+    contar(elemento, contador);
+}
+function alertarTemp(alerta) {
+    if (alerta) {
         document.getElementsByClassName("alerta").item(0).innerHTML = "¡CUIDADO!";
         document.getElementById("Temperatura").setAttribute("style", "filter: grayscale(100%) brightness(40%) sepia(100%) hue-rotate(-50deg) saturate(600%) contrast(0.8);");
 
@@ -21,13 +91,16 @@ function alertarTemp() {
     }
 }
 
-function alertarLuz() {
-    if (luz >= 100) {
+function alertarLuz(alerta) {
+    if (alerta) {
         document.getElementsByClassName("alerta").item(1).innerHTML = "¡CUIDADO!";
         document.getElementById("Luminosidad").setAttribute("style", "filter: grayscale(100%) brightness(40%) sepia(100%) hue-rotate(-50deg) saturate(600%) contrast(0.8);");
 
     }
-}
+else{
+    document.getElementsByClassName("alerta").item(1).innerHTML = "";
+    document.getElementById("Luminosidad").setAttribute("style","filter: ;");
+}}
 
 function eventoAire() {
     if (aire) {
@@ -60,7 +133,7 @@ function eventoPersiana() {
     }
 }
 
-function convertirDia(dia){
+function convertirDia(dia) {
     var semana = new Array(7);
     semana[0] = "Lunes";
     semana[1] = "Martes";
@@ -72,12 +145,12 @@ function convertirDia(dia){
     return semana[dia];
 }
 
-function convertirNumero(n){
+function convertirNumero(n) {
     var resultado;
-    if(n < 10 && n>=0){
-        resultado = "0"+n;
+    if (n < 10 && n >= 0) {
+        resultado = "0" + n;
     }
-    else{
+    else {
         resultado = n;
     }
     return resultado;
@@ -85,23 +158,24 @@ function convertirNumero(n){
 function setTemperatura() {
     var x = document.getElementById("temp").value;
     temperatura = parseInt(x);
-    document.getElementById("tempsensor").innerHTML = temperatura;
-    alertarTemp();
-    
+//    document.getElementById("tempsensor").innerHTML = temperatura;
+
     var t = new Date();
-    t = convertirDia(t.getDay())+ ", " + t.getDate() + " " + t.getMonth()  + " " + t.getFullYear()  + " | " + convertirNumero(t.getHours())  + ":" + convertirNumero(t.getMinutes())  + ":" + convertirNumero(t.getSeconds());
+    t = convertirDia(t.getDay()) + ", " + t.getDate() + "/" + t.getMonth() + "/" + t.getFullYear() + " | " + convertirNumero(t.getHours()) + ":" + convertirNumero(t.getMinutes()) + ":" + convertirNumero(t.getSeconds());
     socket.emit('nuevaTemp', { temperatura, t });
 }
 
 function setLuz() {
     var x = document.getElementById("luz").value;
     luz = parseInt(x);
-    document.getElementById("luzsensor").innerHTML = x;
-    alertarLuz();
+
+    var t = new Date();
+    t = convertirDia(t.getDay()) + ", " + t.getDate() + "/" + t.getMonth() + "/" + t.getFullYear() + " | " + convertirNumero(t.getHours()) + ":" + convertirNumero(t.getMinutes()) + ":" + convertirNumero(t.getSeconds());
+    socket.emit('nuevaLuz', { luz, t });
 }
 
 function actualizarConexiones(usuarios) {
-    var listContainer = document.getElementsByClassName('mensajes').item(2);
+    var listContainer = document.getElementsByClassName('mensajes').item(1);
     listContainer.innerHTML = '';
     var listElement = document.createElement('ol');
     listContainer.appendChild(listElement);
@@ -114,8 +188,8 @@ function actualizarConexiones(usuarios) {
 }
 
 
-function actualizarHistorico(temperaturas) {
-    var listContainer = document.getElementsByClassName('mensajes').item(0);
+function actualizarHistoricoTemps(temperaturas) {
+    var listContainer = document.getElementsByClassName('historicopar').item(0);
     listContainer.innerHTML = '';
     var listElement = document.createElement('ul');
     listContainer.appendChild(listElement);
@@ -127,6 +201,95 @@ function actualizarHistorico(temperaturas) {
     }
 }
 
+
+function actualizarHistoricoLuces(luces) {
+
+    var listContainer = document.getElementsByClassName('historicopar').item(1);
+    listContainer.innerHTML = '';
+    var listElement = document.createElement('ul');
+    listContainer.appendChild(listElement);
+    var num = luces.length;
+    for (var i = 0; i < num; i++) {
+        var listItem = document.createElement('li');
+        listItem.innerHTML = luces[i].t + " - " + luces[i].luz;
+        listElement.appendChild(listItem);
+    }
+}
+function actualizarChat(datos) {
+    var listContainer = document.getElementsByClassName('mensajes').item(0);
+    listContainer.innerHTML = '';
+    var num = datos.length;
+    for (var i = 0; i < num; i++) {
+        var listItem = document.createElement('div');
+        listItem.innerHTML = datos[i].msg + " - " + datos[i].client;
+        listContainer.appendChild(listItem);
+    }
+}
+
+//Contamos los caractéres
+function contar(elemento, contador) {
+    comentario = elemento.value;
+    longitudrestante = longitudmax - comentario.length;
+    contador.innerHTML = longitudrestante;
+
+    if (longitudrestante > 200) {
+        contador.style.backgroundColor = "#98ff8f";
+        contador.style.color = "#085d00";
+    }
+    else if (longitudrestante > 50) {
+        contador.style.backgroundColor = "#ffc67e";
+        contador.style.color = "#d84e00";
+    }
+    else {
+        contador.style.backgroundColor = "#ff6969";
+        contador.style.color = "#b50000";
+    }
+    return false;
+}
+
+function censurar(elemento) {
+    comentario = elemento.value;
+
+    let censuradas = [];
+    censuradas.push("xbox", "playsation", "mierda", "coño", "steam", "polla", "puta", "puto", "maricon", "cabron");
+    for (let i = 0; i < censuradas.length; i++) {
+        let indice = comentario.indexOf(censuradas[i]);
+        if (indice != -1) {
+            comentario = comentario.replace(censuradas[i], '****');
+            elemento.value = comentario;
+        }
+    }
+    return false;
+}
+
+function revisarMensaje(mensaje) {
+    if (mensaje.length == 0 || mensaje.length > longitudmax) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+
+function enviarMensaje(elemento) {
+    var mensaje = elemento.value;
+    console.log("Mensaje" + mensaje);
+    if (revisarMensaje(mensaje)) {
+        socket.emit('chat', mensaje);
+    }
+
+}
+
+function scroll() {
+    var element = document.getElementsByClassName("mensajes").item(0);
+    element.scrollTop = element.scrollHeight;
+}
+
+
+function enviarEventoDia(){
+    socket.emit
+}
 
 socket.on('ack', function (data) {
     console.log(data);
@@ -145,13 +308,25 @@ socket.on('disconnect', function () {
 socket.on('envioTemp', function (data) {
     temperatura = data.temperatura;
     document.getElementById("tempsensor").innerHTML = temperatura;
-    alertarTemp();
 
 });
 
-socket.on('envioHistorico', function(data){
-    console.log(data[0].temperatura);
-    actualizarHistorico(data);
+socket.on('envioLuz', function (data) {
+    luz = data.luz;
+    console.log("LUUUUZ" + luz);
+    document.getElementById("luzsensor").innerHTML = luz + "%";
+
+});
+
+
+socket.on('envioHistoricoTemps', function (data) {
+    actualizarHistoricoTemps(data);
+
+});
+
+
+socket.on('envioHistoricoLuces', function (data) {
+    actualizarHistoricoLuces(data);
 
 });
 
@@ -187,3 +362,14 @@ socket.on('aire', function (data) {
 });
 
 
+socket.on('chat', function (data) {
+    actualizarChat(data);
+});
+
+socket.on('alertatemp', function(data){
+    alertarTemp(data);
+});
+
+socket.on('alertaluz', function(data){
+    alertarLuz(data);
+});
