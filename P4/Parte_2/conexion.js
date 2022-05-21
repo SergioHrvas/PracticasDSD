4059495878c9921ce1b8d9f3614534f505483be4
Sergio <sergiohervas18@gmail.com>
2022-05-22 01:54:00 +0200

    Casi terminado

--- P4/Parte_2/conexion.js
+++ P4/Parte_2/conexion.js
@@ -4,6 +4,9 @@ var fs = require("fs");
 var path = require("path");
 var socketio = require("socket.io");
 const { emit } = require("process");
+var QRCode = require('qrcode')
+
+
 
 var MongoClient = require('mongodb').MongoClient;
 var MongoServer = require('mongodb').Server;
@@ -50,7 +53,7 @@ var httpServer = http.createServer(
 var ventana = true, aire = true;
 var temperatura = 23, luz = 100;
 var temperatura_fija = 23;
-
+const MAXIMA_TEMP = 50, MAXIMA_LUZ = 100, MINIMA_TEMP = -10;
 
 var allClients = new Array();
 
@@ -64,25 +67,61 @@ MongoClient.connect("mongodb://localhost:27017/", { useUnifiedTopology: true }, 
 	let collectionchat = dbo.collection("chat");
 	let collectioncalendario = dbo.collection("calendario");
 
+	//Verifica los fallos de la temperatura y de la luz. En el caso de que se produzcan, enviará un mensaje para cerrar la persiana.
 	function verificarFallos() {
-		if ((temperatura < -10 || temperatura >= 50) && luz > 100) {
+		if ((temperatura < MINIMA_TEMP || temperatura >= MAXIMA_TEMP) && luz > MAXIMA_LUZ) {
 			ventana = false;
 			io.sockets.emit('persiana', ventana);
 			io.sockets.emit('alertaluz', false);
-
 		}
 	}
 
+	//Alertas de temperatura y luz en el caso de que se excedan los límites
 	function envioAlertaTemp() {
 		var alerta = false;
-		if (temperatura >= 40 || temperatura <= 0) {
+		if (temperatura >= MAXIMA_TEMP-10 || temperatura <= MINIMA_TEMP+10) {
 			alerta = true;
 		}
-
 		io.sockets.emit('alertatemp', alerta);
-
 	}
 
+	function envioAlertaLuz() {
+		var alerta = false;
+		if (luz >= MAXIMA_LUZ) {
+			alerta = true;
+		}
+		io.sockets.emit('alertaluz', alerta);
+	}
+
+
+	function buscarTemps(){
+		setTimeout(function () {
+			collectiontemps.find().toArray(function (err, results) {
+				if (err) {
+					console.log("ERROR");
+				}
+				else {
+					io.sockets.emit('envioHistoricoTemps', results);
+				}
+
+			});
+		}, 500);
+	}
+
+	function buscarLuces(){
+		setTimeout(function () {
+			collectionluces.find().toArray(function (err, results) {
+				if (err) {
+					console.log("ERROR");
+				}
+				else {
+					io.sockets.emit('envioHistoricoLuces', results);
+				}
+
+			});
+		}, 500);
+	}
+	//Función que se repite cada 5 segundos. Irá actualizando la temperatura de forma automática como si fuese un simulador.
 	setInterval(function () {
 		if (aire) {
 			if (temperatura < temperatura_fija) {
@@ -92,46 +131,51 @@ MongoClient.connect("mongodb://localhost:27017/", { useUnifiedTopology: true }, 
 				temperatura -= 1;
 			}
 		}
-		else if (temperatura < 50 && !aire) {
-			temperatura += 1;
-
+		else if (!aire) {
+			if(temperatura < MAXIMA_TEMP)
+				temperatura += 1;
+			else if(temperatura >= MAXIMA_TEMP){
+				temperatura -= 1;
+			}
 		}
 		var d = new Date();
 		var t = d.getTime();
 		io.sockets.emit('envioTemp', { temperatura, t });
 		envioAlertaTemp();
-	}
-		, 5000);
-
-
-
-	setTimeout(function () {
 	}, 5000);
+
+	//Cuando se reciba una conexión nueva de un cliente
 	io.sockets.on('connection',
 		function (client) {
+
+			//El servidor responde con un ack confirmando la recepción de la conexión
 			client.emit('ack', "Conexión recibida. Te respondo.");
 
+			//Cuando el servidor reciba un ack del cliente, significará que el cliente recibió el ack y ha informado de su conexión satisfactoria.
+			//Por tanto, añadirá el nuevo cliente al vector de clientes disponibles y enviará dicho array a todos los clientes (para mostrar las conexiones posteriormente).
 			client.on('ack', function (data) {
 				console.log(data);
 				allClients.push({ address: client.request.connection.remoteAddress, port: client.request.connection.remotePort });
 				io.sockets.emit('all-connections', allClients);
 			});
 
+			//Cuando el servidor reciba un mensaje 'aire' significará que ha cambiado el estado del aire acondicionado en un cliente
+			//y deberá ser cambiado en el propio servidor y en todos los demás clientes, por lo que enviará un mensaje 'aire' a todos ellos.
 			client.on('aire', function (data) {
-				console.log(`Aire: ${data.cambio}`);
 				aire = data.cambio;
 				io.sockets.emit('aire', aire);
 			});
 
+			//Igual que el anterior pero con la persiana
 			client.on('persiana', function (data) {
-				console.log(`Persiana: ${data.cambio}`);
 				ventana = data.cambio;
 				io.sockets.emit('persiana', ventana);
 			});
 
+			//Cuando el servidor reciba un mensaje 'disconnect', busca cual de todos ha sido y 
+			//lo saca de la lista. Entonces, envía de nuevo el vector a todos los clientes sin el que se ha desconectado.
 			client.on('disconnect', function () {
 				console.log("El cliente " + client.request.connection.remoteAddress + " se va a desconectar");
-
 				var index = -1;
 				for (var i = 0; i < allClients.length; i++) {
 					if (allClients[i].address == client.request.connection.remoteAddress
@@ -139,7 +183,6 @@ MongoClient.connect("mongodb://localhost:27017/", { useUnifiedTopology: true }, 
 						index = i;
 					}
 				}
-
 
 				if (index != -1) {
 					allClients.splice(index, 1);
@@ -150,21 +193,19 @@ MongoClient.connect("mongodb://localhost:27017/", { useUnifiedTopology: true }, 
 				console.log('El usuario ' + client.request.connection.remoteAddress + ' se ha desconectado');
 			});
 
-			client.on('nuevaTemp', function (data) {
+			client.on('inicioHistoricos', function(){
+				buscarTemps();
+				buscarLuces();
+			});
+			//Cuando el servidor reciba un mensaje de 'envioTemp', significará que el cliente ha modificado el valor por teclado de la temperatura,
+			//y por tanto actualizará su sensor, lo meterá en la base de datos y mandará el mensaje con el nuevo aire a todos los clientes para que modifiquen dicho valor, además del histórico
+			//de temperaturas para que actualice dicho valor
+			//Por último, enviará una alerta si la temperatura recibida supera cierto umbral.
+			client.on('envioTemp', function (data) {
 				temperatura = data.temperatura;
 				if (collectiontemps != null) {
 					collectiontemps.insertOne(data, { safe: true }, function (err, result) { });
-					setTimeout(function () {
-						collectiontemps.find().toArray(function (err, results) {
-							if (err) {
-								console.log("ERROR");
-							}
-							else {
-								io.sockets.emit('envioHistoricoTemps', results);
-							}
-
-						});
-					}, 500);
+					buscarTemps();
 
 				}
 				io.sockets.emit('envioTemp', data);
@@ -173,40 +214,25 @@ MongoClient.connect("mongodb://localhost:27017/", { useUnifiedTopology: true }, 
 				verificarFallos();
 			});
 
-			client.on('nuevaLuz', function (data) {
+			//Igual pero con la luz
+			client.on('envioLuz', function (data) {
 				luz = data.luz;
 				if (collectionluces != null) {
 					collectionluces.insertOne(data, { safe: true }, function (err, result) { });
-					setTimeout(function () {
-
-						collectionluces.find().toArray(function (err, results) {
-							if (err) {
-								console.log("ERROR");
-							}
-							else {
-								io.sockets.emit('envioHistoricoLuces', results);
-							}
-
-						});
-					}, 500);
+					buscarLuces();
 				}
 				io.sockets.emit('envioLuz', data);
-				var alerta = false;
-				if (luz >= 100) {
-					alerta = true;
-				}
-				io.sockets.emit('alertaluz', alerta);
+				envioAlertaLuz();
 				verificarFallos();
 
 			});
 
-
+			//El servidor al recibir un mensaje 'chat', inserta los datos en la colección y extrae todos los elementos de la colección, mandándolos a todos los clientes.
 			client.on('chat', function (data) {
 				var datos = { msg: data, client: client.request.connection.remotePort }
 				if (collectionchat != null) {
 					collectionchat.insertOne(datos);
 					setTimeout(function () {
-						console.log("I am the third log after 5 seconds");
 
 						collectionchat.find().toArray(function (err, results) {
 							if (err) {
@@ -222,6 +248,8 @@ MongoClient.connect("mongodb://localhost:27017/", { useUnifiedTopology: true }, 
 				}
 			});
 
+			//El servidor al recibir un mensaje 'calendario', inserta los datos en la colección y extrae los datos que correspondan a ese único día.
+			//Tras esto, envía los resultados de la búsqueda al cliente que envió el mensaje
 			client.on('calendario', function (data) {
 				var datos = { fecha: data.fecha, msg: data.mensaje }
 				if (collectioncalendario != null) {
@@ -241,22 +269,27 @@ MongoClient.connect("mongodb://localhost:27017/", { useUnifiedTopology: true }, 
 				}
 			});
 
-
+			//Realiza lo mismo que el anterior pero sin insertar datos.
 			client.on('getEvento', function (data) {
 				if (collectioncalendario != null) {
-						collectioncalendario.find({ fecha: data }).toArray(function (err, results) {
-							if (err) {
-								console.log("ERROR");
-							}
-							else {
-								client.emit('calendario', results);
-							}
+					collectioncalendario.find({ fecha: data }).toArray(function (err, results) {
+						if (err) {
+							console.log("ERROR");
+						}
+						else {
+							client.emit('calendario', results);
+						}
 
-						});
+					});
 
 				}
+
 			});
 
+			//Si recibe un mensaje 'qr', lo reenvía a todos los clientes
+			client.on('qr', function (data) {
+				io.sockets.emit('qr', data);
+			})
 		});
 });
 
--- P4/Parte_2/domotica.js
+++ P4/Parte_2/domotica.js
@@ -1,51 +1,51 @@
+
+//Importamos paquetes
+var QRCode = require('qrcode')
+var canvas = document.getElementById('canvas')
+
+//Declaramos variables
+let longitudmax = 1000, longitudrestante = 1000;
+var ventana = true, aire = true;
+var temperatura = 23, luz = 100;
+const meses = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+var fecha = new Date();
+var fechaselect;
+var diaSeleccionado, mesSeleccionado, añoSeleccionado;
+var serviceURL = document.URL;
+var socket = io.connect(serviceURL);
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const mesesNombres = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+//Añadimos listeners
 document.getElementById("AireAcon").addEventListener('click', function () { eventoAire() });
 document.getElementById("Persiana").addEventListener('click', function () { eventoPersiana() });
+
 document.getElementById("enviar1").addEventListener('click', function () { setTemperatura() });
 document.getElementById("enviar2").addEventListener('click', function () { setLuz() });
+
 document.getElementById("enviarmensaje").addEventListener('click', function () { enviarMensaje(document.getElementById("mensaje")); });
 document.getElementById("mensaje").addEventListener("keyup", function () { evaluarComentario(this, document.getElementById("contadormensaje")) });
 
 document.getElementById("enviarnota").addEventListener('click', function () { enviarMensaje(document.getElementById("nota")); });
 document.getElementById("nota").addEventListener("keyup", function () { evaluarComentario(this, document.getElementById("contadornota")) });
 
+document.getElementById("enviarqr").addEventListener('click', function () { enviarQR(document.getElementById("nota")); });
+
 document.getElementById("flechaizq").addEventListener('click', function () { izquierda(); });
 document.getElementById("flechader").addEventListener("click", function () { derecha(); });
 
-
 document.addEventListener('DOMContentLoaded', function () { pintarMeses() });
-
-let longitudmax = 1000, longitudrestante = 1000;
-var ventana = true, aire = true;
-var temperatura = 23, luz = 100;
-const meses = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-var fecha = new Date();
-
-var diaSeleccionado, mesSeleccionado, añoSeleccionado;
-var serviceURL = document.URL;
-var socket = io.connect(serviceURL);
+document.addEventListener('DOMContentLoaded', function () { iniciarHistoricos() });
 
 contar(document.getElementById("enviarmensaje"), document.getElementById("contadormensaje"));
 contar(document.getElementById("enviarnota"), document.getElementById("contadornota"));
 
+//Scrolleamos constantemente el chat para estar siempre abajo
 setInterval(function () {
     scroll()
 }, 100);
 
-const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-const mesesNombres = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-
-function derecha(){
-    fecha.setMonth(fecha.getMonth()+1)
-
-    pintarMeses();
-}
-
-function izquierda(){
-    fecha.setMonth((fecha.getMonth()-1)%12)
-
-    pintarMeses();
-}
-
+//Para la navegación entre los distintos meses y años
 function pintarMeses() {
     var mes = document.getElementById("mes");
     document.getElementById("fechamensaje").innerHTML=mesesNombres[fecha.getMonth()]+"/"+fecha.getFullYear();
@@ -58,9 +58,11 @@ function pintarMeses() {
     mesSeleccionado = nummes;
     añoSeleccionado = fecha.getFullYear();
 
+    fechaselect = diaSeleccionado + "/" + mesesNombres[mesSeleccionado] + "/" + añoSeleccionado;
+    document.getElementById("fecha").innerHTML = fechaselect;
+    
     var dia = (primerdia.getDay() + 6) % 7;
     var diasmes = meses[nummes] + dia;
-
 
     var contador = 1;
     for (var d = 0; d < diasmes; d++) {
@@ -76,31 +78,44 @@ function pintarMeses() {
             diaelement.innerHTML = contador;
             contador++;
             diaelement.addEventListener('click', function () { mostrarform(this.innerHTML) });
-
         }
         mes.appendChild(diaelement);
     }
 }
 
+//Pasamos al mes siguiente
+function derecha(){
+    fecha.setMonth(fecha.getMonth()+1)-
+    pintarMeses();
+}
 
+//Pasamos al mes anterior
+function izquierda(){
+    fecha.setMonth((fecha.getMonth()-1)%12)
+    pintarMeses();
+}
+
+//Mostramos las notas de la fecha seleccionada y el cuadro de texto para enviar una nota
 function mostrarform(dia) {
     diaSeleccionado = parseInt(dia);
     document.getElementById("cajamensajes").style.display = "initial";
-    fecha = diaSeleccionado + "/" + mesesNombres[mesSeleccionado] + "/" + añoSeleccionado;
-    document.getElementById("fecha").innerHTML = fecha;
+    fechaselect = diaSeleccionado + "/" + mesesNombres[mesSeleccionado] + "/" + añoSeleccionado;
+    document.getElementById("fecha").innerHTML = fechaselect;
     var a = document.getElementById("fecha").innerHTML;
-    socket.emit('getEvento', fecha);
+    socket.emit('getEvento', fechaselect);
 }
 
+//Evaluamos comentario para evitar palabras malsonantes y contar los caracteres
 function evaluarComentario(elemento, contador) {
     censurar(elemento);
     contar(elemento, contador);
 }
+
+//Creamos la alerta si la temperatura excede un límite
 function alertarTemp(alerta) {
     if (alerta) {
         document.getElementsByClassName("alerta").item(0).innerHTML = "¡CUIDADO!";
         document.getElementById("Temperatura").setAttribute("style", "filter: grayscale(100%) brightness(40%) sepia(100%) hue-rotate(-50deg) saturate(600%) contrast(0.8);");
-
     }
     else {
         document.getElementsByClassName("alerta").item(0).innerHTML = "";
@@ -108,11 +123,11 @@ function alertarTemp(alerta) {
     }
 }
 
+//Creamos la alerta si la luz excede un límite
 function alertarLuz(alerta) {
     if (alerta) {
         document.getElementsByClassName("alerta").item(1).innerHTML = "¡CUIDADO!";
         document.getElementById("Luminosidad").setAttribute("style", "filter: grayscale(100%) brightness(40%) sepia(100%) hue-rotate(-50deg) saturate(600%) contrast(0.8);");
-
     }
     else {
         document.getElementsByClassName("alerta").item(1).innerHTML = "";
@@ -120,37 +135,35 @@ function alertarLuz(alerta) {
     }
 }
 
+//Apagamos o encendemos aire acondicionado
 function eventoAire() {
     if (aire) {
         socket.emit('aire', { cambio: false });
-        //      document.getElementById("tempsensor").innerHTML = temperatura;
         document.getElementById("AireAcon").setAttribute("style", "-webkit-filter:grayscale(100%)");
     }
     else {
         socket.emit('aire', { cambio: true });
         document.getElementById("AireAcon").setAttribute("style", "-webkit-filter:grayscale(0%)");
-
     }
-
 }
 
+//Subimos o bajamos la persiana
 function eventoPersiana() {
     if (!ventana) {
         socket.emit('persiana', { cambio: true });
         luz = 100;
         document.getElementById("luzsensor").innerHTML = luz + "%";
         document.getElementById("Persiana").setAttribute("style", "-webkit-filter:grayscale(0%)");
-
     }
     else {
         socket.emit('persiana', { cambio: false });
         luz = 0;
         document.getElementById("luzsensor").innerHTML = luz + "%";
         document.getElementById("Persiana").setAttribute("style", "-webkit-filter:grayscale(100%)");
-
     }
 }
 
+//Convertimos el día de la semana en número a cadena de caracteres
 function convertirDia(dia) {
     var semana = new Array(7);
     semana[0] = "Lunes";
@@ -163,6 +176,7 @@ function convertirDia(dia) {
     return semana[dia];
 }
 
+//Convertimos número de un dígito a dos (ej: 1 -> 01)
 function convertirNumero(n) {
     var resultado;
     if (n < 10 && n >= 0) {
@@ -173,25 +187,33 @@ function convertirNumero(n) {
     }
     return resultado;
 }
+
+//Establecemos valor de temperatura
 function setTemperatura() {
     var x = document.getElementById("temp").value;
     temperatura = parseInt(x);
-    //    document.getElementById("tempsensor").innerHTML = temperatura;
 
     var t = new Date();
     t = convertirDia(t.getDay()) + ", " + t.getDate() + "/" + t.getMonth() + "/" + t.getFullYear() + " | " + convertirNumero(t.getHours()) + ":" + convertirNumero(t.getMinutes()) + ":" + convertirNumero(t.getSeconds());
-    socket.emit('nuevaTemp', { temperatura, t });
+    socket.emit('envioTemp', { temperatura, t });
 }
 
+//Establecemos valor de luz
 function setLuz() {
     var x = document.getElementById("luz").value;
     luz = parseInt(x);
 
     var t = new Date();
     t = convertirDia(t.getDay()) + ", " + t.getDate() + "/" + t.getMonth() + "/" + t.getFullYear() + " | " + convertirNumero(t.getHours()) + ":" + convertirNumero(t.getMinutes()) + ":" + convertirNumero(t.getSeconds());
-    socket.emit('nuevaLuz', { luz, t });
+    socket.emit('envioLuz', { luz, t });
 }
 
+//Iniciamos los históricos en cuanto el documento se cargue
+function iniciarHistoricos(){
+    socket.emit('inicioHistoricos');
+}
+
+//Actualizamos la lista de usuarios conectados
 function actualizarConexiones(usuarios) {
     var listContainer = document.getElementsByClassName('mensajes').item(1);
     listContainer.innerHTML = '';
@@ -205,7 +227,7 @@ function actualizarConexiones(usuarios) {
     }
 }
 
-
+//Actualizamos la lista de temperaturas
 function actualizarHistoricoTemps(temperaturas) {
     var listContainer = document.getElementsByClassName('historicopar').item(0);
     listContainer.innerHTML = '';
@@ -219,9 +241,8 @@ function actualizarHistoricoTemps(temperaturas) {
     }
 }
 
-
+//Actualizamos la lista de luces
 function actualizarHistoricoLuces(luces) {
-
     var listContainer = document.getElementsByClassName('historicopar').item(1);
     listContainer.innerHTML = '';
     var listElement = document.createElement('ul');
@@ -234,6 +255,7 @@ function actualizarHistoricoLuces(luces) {
     }
 }
 
+//Actualizamos chat
 function actualizarChat(datos) {
     var listContainer = document.getElementsByClassName('mensajes').item(0);
     listContainer.innerHTML = '';
@@ -245,12 +267,12 @@ function actualizarChat(datos) {
     }
 }
 
+//Actualizamos calendario
 function actualizarCalendario(datos) {
     var listContainer = document.getElementsByClassName('mensajes').item(3);
     var fecha = document.getElementById('fecha');
     listContainer.innerHTML = "";
     listContainer.appendChild(fecha);
-
     var num = datos.length;
     for (var i = 0; i < num; i++) {
         var listItem = document.createElement('div');
@@ -258,6 +280,7 @@ function actualizarCalendario(datos) {
         listContainer.appendChild(listItem);
     }
 }
+
 //Contamos los caractéres
 function contar(elemento, contador) {
     comentario = elemento.value;
@@ -279,9 +302,9 @@ function contar(elemento, contador) {
     return false;
 }
 
+//Censuramos palabras
 function censurar(elemento) {
     comentario = elemento.value;
-
     let censuradas = [];
     censuradas.push("xbox", "playsation", "mierda", "coño", "steam", "polla", "puta", "puto", "maricon", "cabron");
     for (let i = 0; i < censuradas.length; i++) {
@@ -294,6 +317,7 @@ function censurar(elemento) {
     return false;
 }
 
+//Revisamos si la longitud del mensaje es correcta
 function revisarMensaje(mensaje) {
     if (mensaje.length == 0 || mensaje.length > longitudmax) {
         return false;
@@ -303,7 +327,7 @@ function revisarMensaje(mensaje) {
     }
 }
 
-
+//Enviamos el mensaje (para el chat o el calendario)
 function enviarMensaje(elemento) {
     var mensaje = elemento.value;
     if (revisarMensaje(mensaje)) {
@@ -315,30 +339,30 @@ function enviarMensaje(elemento) {
             socket.emit('calendario', {mensaje,fecha});
         }
     }
-
 }
 
+//Realizamos el scroll del chat hacia abajo
 function scroll() {
     var element = document.getElementsByClassName("mensajes").item(0);
     element.scrollTop = element.scrollHeight;
 }
 
-
-function enviarEventoDia() {
-    socket.emit
+//Enviamos código QR
+function enviarQR(){
+    QRCode.toCanvas(canvas, document.getElementById('qrtexto').value, function (error) {
+        if (error) console.error(error)
+        socket.emit(canvas.innerHTML);
+        canvas.style.width = "300px";
+        canvas.style.height = "300px";
+      })
+      
 }
 
 socket.on('ack', function (data) {
     socket.emit('ack', "Conexion establecida correctamente!")
-
-    //socket.emit('obtener', { host: data.address });
 });
 socket.on('all-connections', function (data) {
     actualizarConexiones(data);
-});
-
-socket.on('disconnect', function () {
-    //actualizarLista({});
 });
 
 socket.on('envioTemp', function (data) {
@@ -410,3 +434,8 @@ socket.on('alertatemp', function (data) {
 socket.on('alertaluz', function (data) {
     alertarLuz(data);
 });
+
+socket.on('qr', function (data){
+    canvas.getElementById.innerHTML = data;
+
+})
